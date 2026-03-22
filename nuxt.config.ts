551872ea35df89109d2054e22e9b2d49a6b772c3
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2026-03-22 21:49:00 +0700

    feat: setup tailwind v4.2.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
+  css: ["./app/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
